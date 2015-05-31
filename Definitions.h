607c7d85f262877234583ff7383fe10e8f35fee0
mpgramlich <mpgramlich@mix.wvu.edu>
2015-05-30 23:43:46 -0400

    Committing initial project

---
+++ Definitions.h
@@ -0,0 +1,15 @@
+/*
+ * Definitions.h
+ *
+ *  Created on: May 29, 2015
+ *      Author: Matt Gramlich
+ *      			Greg Lusk
+ */
+
+#ifndef DEFINITIONS_H_
+#define DEFINITIONS_H_
+
+
+
+
+#endif /* DEFINITIONS_H_ */
